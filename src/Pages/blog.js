5df1332efa053847a506f638bdaf2c51affa641d
Tulika2235 <btbtn19058_tulika@banasthali.in>
2022-04-21 21:02:14 +0530

    update

---
+++ src/Pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout/Layout"
+
+const blog = () => {
+  return (
+    <Layout>
+      <h1 className="text-success">blog</h1>
+    </Layout>
+  )
+}
+
+export default blog
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,109 +1,72 @@
 import React from "react"
-import {
-  Bootstrap,
-  Calendar3,
-  CpuFill,
-  GeoFill,
-  House,
-  Speedometer,
-  Speedometer2,
-  Toggles2,
-  Tools,
-} from "react-bootstrap-icons"
+import { Link } from "gatsby"
+import { Facebook, Instagram, Linkedin } from "react-bootstrap-icons"
 import Image from "../Images/Images"
 
 const Items = [
   {
-    icon: <Bootstrap />,
-    title: "bootstrap title",
-    description: "Bootstrap description paragraph.",
+    name: "member1.jpg",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com",
+    linkedin: "https://www.linkedin.com",
   },
-
   {
-    icon: <CpuFill />,
-    title: "Calendar3",
-    description: "Calendar3 Calendar3 Calendar3",
+    name: "member2.jpg",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com",
+    linkedin: "https://www.linkedin.com",
   },
-
   {
-    icon: <GeoFill />,
-    title: "Calendar3",
-    description: "Calendar3 Calendar3 Calendar3",
+    name: "member3.jpg",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com",
+    linkedin: "https://www.linkedin.com",
   },
-
   {
-    icon: <House />,
-    title: "Calendar3",
-    description: "Calendar3 Calendar3 Calendar3",
-  },
-
-  {
-    icon: <Speedometer />,
-    title: "Calendar3",
-    description: "Calendar3 Calendar3 Calendar3",
-  },
-
-  {
-    icon: <Speedometer2 />,
-    title: "Calendar3",
-    description: "Calendar3 Calendar3 Calendar3",
-  },
-
-  {
-    icon: <Toggles2 />,
-    title: "Calendar3",
-    description: "Calendar3 Calendar3 Calendar3",
-  },
-
-  {
-    icon: <Tools />,
-    title: "Calendar3",
-    description: "Calendar3 Calendar3 Calendar3",
+    name: "member4.jpg",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com",
+    linkedin: "https://www.linkedin.com",
   },
 ]
 
 const Team = () => {
   return (
     <div className="container text-center py-5">
-      <h2 class="pb-2 border-bottom text-center">Team Members</h2>
-      <div className="row justify-content-center">
-        <div className="col-3">
+      <h2 className="pb-2 border-bottom text-center">Team Members</h2>
+      <div className="row justify-content-center py-5">
+        {Items.map((item, i) => {
+          return (
+            <div key={i} className="col-lg-3 col-md-6 col-sm-12">
+              <div className="card card-body shadow mb-4">
+                <Image
+                  className="d-block mx-lg-auto img-fluid"
+                  style={{
+                    margin: "0 auto",
+                    height: "200px",
+                    width: "200px",
+                    borderRadius: "100%",
+                  }}
+                  filename={`${item.name}.jpg`}
+                  alt="1"
+                />
+                <h3 className="m-2">{item.name}</h3>
 
-            
-          <div className="card card-body">
-            <Image
-              className="d-block mx-lg-auto img-fluid"
-              style={{ height: "200px", width: "200px", borderRadius: "50%" }}
-              filename="Member 1.jpg"
-              alt="1"
-            />
-            {/* <h5>Member1</h5> */}
-          </div>
-        </div>
-
-        {/* <div className="col-3">
-          <Image
-            className="d-block mx-lg-auto img-fluid"
-            filename="Member2.jpg"
-            alt="2"
-          />
-        </div>
-
-        <div className="col-3">
-          <Image
-            className="d-block mx-lg-auto img-fluid"
-            filename="Member3.jpg"
-            alt="3"
-          />
-        </div>
-
-        <div className="col-3">
-          <Image
-            className="d-block mx-lg-auto img-fluid"
-            filename="Member4.jpg"
-            alt="4"
-          />
-        </div> */}
+                <ul className="list-unstyled justify-content-center d-flex mb-0">
+                  <Link className="m-3" to={item.facebook}>
+                    <Facebook />
+                  </Link>
+                  <Link className="m-3" to={item.instagram}>
+                    <Instagram />
+                  </Link>
+                  <Link className="m-3" to={item.linkedin}>
+                    <Linkedin />
+                  </Link>
+                </ul>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
